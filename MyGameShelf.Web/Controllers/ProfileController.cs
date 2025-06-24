d55f520f41e244835c4c89963bb930bbd7c926bd
Dennis <dmarlo96@live.ca>
2025-06-24 16:35:36 -0400

    Profile Page

---
+++ MyGameShelf.Web/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyGameShelf.Web.Controllers;
+
+[Route("profile")]
+public class ProfileController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
