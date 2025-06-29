99f6a686bfe2cc2208fc8efc373ae7fa3d310bbc
Dennis <dmarlo96@live.ca>
2025-06-29 18:34:24 -0400

    Photo Service custom exception

---
+++ MyGameShelf.Application/Exceptions/PhotoServiceException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGameShelf.Application.Exceptions;
+
+public class PhotoServiceException : Exception
+{
+    public PhotoServiceException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
