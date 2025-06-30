cb37a034a806ef88e9e3d6947c27d697f5e41aee
Dennis <dmarlo96@live.ca>
2025-06-30 16:48:16 -0400

    New Migration: Model changes and validation

---
+++ MyGameShelf.Infrastructure/Migrations/20250630204738_ModelValidation.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyGameShelf.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelValidation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserGames_UserId",
+                table: "UserGames");
+
+            migrationBuilder.DropColumn(
+                name: "AddedOn",
+                table: "UserGames");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Reviews");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Rating",
+                table: "Reviews",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGames_UserId_GameId",
+                table: "UserGames",
+                columns: new[] { "UserId", "GameId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserGames_UserId_GameId",
+                table: "UserGames");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AddedOn",
+                table: "UserGames",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Rating",
+                table: "Reviews",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Reviews",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGames_UserId",
+                table: "UserGames",
+                column: "UserId");
+        }
+    }
+}
