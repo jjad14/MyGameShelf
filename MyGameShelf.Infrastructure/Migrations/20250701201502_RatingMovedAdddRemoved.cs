19c95f345e49f0f2fd3244b1b7a79c296cfe5c4c
Dennis <dmarlo96@live.ca>
2025-07-01 16:17:12 -0400

    New Migration - changed review rating to isRecommended and added rating to UserGame, removed added from Game entity

---
+++ MyGameShelf.Infrastructure/Migrations/20250701201502_RatingMovedAdddRemoved.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyGameShelf.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RatingMovedAdddRemoved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "Added",
+                table: "Games");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Rating",
+                table: "UserGames",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRecommended",
+                table: "Reviews",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "UserGames");
+
+            migrationBuilder.DropColumn(
+                name: "IsRecommended",
+                table: "Reviews");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Rating",
+                table: "Reviews",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Added",
+                table: "Games",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
