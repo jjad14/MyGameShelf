41aca1b909017848b5c3d9f49c053744fb20e7d0
Dennis <dmarlo96@live.ca>
2025-07-02 12:47:27 -0400

    New migration, Unique games in Game Entity

---
+++ MyGameShelf.Infrastructure/Migrations/20250702164008_UniqueGameRawgId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyGameShelf.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueGameRawgId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_RawgId",
+                table: "Games",
+                column: "RawgId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Games_RawgId",
+                table: "Games");
+        }
+    }
+}
