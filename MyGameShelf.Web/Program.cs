1d4b38d3eb769a259920df803dd8980cd2c1c19c
Dennis <dmarlo96@live.ca>
2025-07-03 16:56:30 -0400

    Registered UserGame Repository

--- MyGameShelf.Web/Program.cs
+++ MyGameShelf.Web/Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IUserGameRepository, UserGameRepository>();
 builder.Services.AddScoped<IGameRepository, GameRepository>();
 builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
 builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
